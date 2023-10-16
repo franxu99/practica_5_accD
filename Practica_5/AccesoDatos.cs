31462d052218f53b54898046e7a658050e0d26d9
franxu99 <110120388+franxu99@users.noreply.github.com>
2023-10-16 20:07:07 +0200

    0.1

---
+++ Practica_5/AccesoDatos.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Security.Cryptography;
+
+namespace Practica_5
+{
+    public class AccesoDatos
+    {
+        String myConnectionString = "server=localhost;" +
+            "port=3306;" +
+            "user=root;" +
+            "database=sakila;" +
+            "password=1234;"
+            ;
+        MySqlConnection _conn;
+        private int _numeroPeliculas;
+        MySqlCommand _cmd = new MySqlCommand();
+        public AccesoDatos()
+        {
+            try
+            {
+                _conn = new MySqlConnection(myConnectionString);
+                _conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    public int AltaDePelicula(string title, string description, int release_year, int language_id, int original_lenguage_id, int rental_duration, double rental_rate, int length, double replacement_cost, string rating, string special_features)
+    {
+        int resultado = -98;
+        try
+        {
+            _cmd = new MySqlCommand();
+            _cmd.Connection = _conn;
+            _cmd.CommandType = CommandType.StoredProcedure;
+            _cmd.CommandText = "InsertaAltaPelicula";
+
+            //Parametros Input
+
+            _cmd.Parameters.AddWithValue("_title", title);
+            _cmd.Parameters["_title"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_description", description);
+            _cmd.Parameters["_description"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_release_year", release_year);
+            _cmd.Parameters["_release_year"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_language_id", language_id);
+            _cmd.Parameters["_language_id"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_original_lenguage_id", original_lenguage_id);
+            _cmd.Parameters["_original_lenguage_id"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_rental_duration", rental_duration);
+            _cmd.Parameters["_rental_duration"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_rental_rate", rental_rate);
+            _cmd.Parameters["_rental_rate"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_length", length);
+            _cmd.Parameters["_length"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_replacement_cost", replacement_cost);
+            _cmd.Parameters["_replacement_cost"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_rating", rating);
+            _cmd.Parameters["_rating"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_special_features", special_features);
+            _cmd.Parameters["_special_features"].Direction = ParameterDirection.Input;
+
+            //Parametros Output
+
+            _cmd.Parameters.Add(new MySqlParameter("_res", MySqlDbType.Int32));
+            _cmd.Parameters["_res"].Direction = ParameterDirection.Output;
+
+            _cmd.ExecuteNonQuery();
+
+            resultado = (int)_cmd.Parameters["_res"].Value;
+            return resultado;
+
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+            return resultado;
+        }
+    }
+    
+    public int AltaEmpleado(string first_name, string last_name, string email, int store_id, string username, string password)
+    {
+        int resultado = -98;
+        try
+        {
+            _cmd = new MySqlCommand();
+            _cmd.Connection = _conn;
+            _cmd.CommandType = CommandType.StoredProcedure;
+            _cmd.CommandText = "InsertUser";
+
+            //Parametros Input
+
+            _cmd.Parameters.AddWithValue("_first_name", first_name);
+            _cmd.Parameters["_first_name"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_last_name", last_name);
+            _cmd.Parameters["_last_name"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_email", email);
+            _cmd.Parameters["_email"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_store_id", store_id);
+            _cmd.Parameters["_store_id"].Direction = ParameterDirection.Input;
+
+            _cmd.Parameters.AddWithValue("_username", username);
+            _cmd.Parameters["_username"].Direction = ParameterDirection.Input;
+
+                password = CreateMD5(password);
+
+            _cmd.Parameters.AddWithValue("_password", password);
+            _cmd.Parameters["_password"].Direction = ParameterDirection.Input;
+
+            //Parametros Output
+
+            _cmd.Parameters.Add(new MySqlParameter("_res", MySqlDbType.Int32));
+            _cmd.Parameters["_res"].Direction = ParameterDirection.Output;
+
+            _cmd.ExecuteNonQuery();
+
+            resultado = (int)_cmd.Parameters["_res"].Value;
+            return resultado;
+
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+            return resultado;
+        }
+    }
+
+    public string CreateMD5(string password)
+        {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(password);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                return Convert.ToHexString(hashBytes);
+            }
+        }
+
+    public int DameNumeroPeliculas()
+        {
+            int res = -98;
+            try
+            {
+                _cmd = new MySqlCommand();
+                _cmd.Connection = _conn;
+                _cmd.CommandType = CommandType.StoredProcedure;
+                _cmd.CommandText = "CuentaPeliculas";
+
+                //OUTPUT
+                _cmd.Parameters.AddWithValue("_res", MySqlDbType.Int32);
+                _cmd.Parameters["_res"].Direction = ParameterDirection.Output;
+                _cmd.ExecuteNonQuery();
+                res = (int)_cmd.Parameters["_res"].Value;
+                return res;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return res;
+            }           
+        }
+
+    }
+}
---
+++ Practica_5/Registro.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Practica_5
+{
+    /// <summary>
+    /// Lógica de interacción para Registro.xaml
+    /// </summary>
+    public partial class Registro : Window
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+        private void Cerrar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Crear_Click(object sender, RoutedEventArgs e)
+        {
+            AccesoDatos ad = new AccesoDatos();
+            int itemStore = 0;
+            if (cbStore.SelectedIndex == 0)
+            {
+                itemStore = 1;
+            }
+            else if (cbStore.SelectedIndex == 1) 
+            { 
+                itemStore = 2;
+            }
+            
+            if(txtPsw.Password != txtConfirmPsw.Password) 
+            {
+                MessageBox.Show("Las contraseñas deben coincidir");
+            }
+            else 
+            {
+                int res = ad.AltaEmpleado(txtName.Text, txtLastName.Text, txtEmail.Text, itemStore, txtUserName.Text, txtPsw.Password);
+                if(res == 0) 
+                {
+                    MessageBox.Show("Introducido con exito");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Ha ocurrido un error");
+                }
+            }
+        }
+    }
+}
